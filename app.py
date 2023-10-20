42846f95e88c5519314db20e6948b106e8a050cd
Claudio Puggioni <76516704+ClaudioPuggioni@users.noreply.github.com>
2023-10-21 01:26:59 +0700

    added back cross_origin for future test of whitelist

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
 # Middleware
+# cross_origin(["http://www.domain1.com", "http://www.domain2.com"])
 cors = CORS(app)
 
 from views import *
